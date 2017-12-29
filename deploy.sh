87516da10e3250d22f39863f048a7c3bcf478386
tongjin.yao <280290357@qq.com>
2017-12-29 17:05:15 +0800

    delete_sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-git checkout .
-git pull
-./mvnw -DskipTests clean install
-STORAGE_TYPE=elasticsearch ES_HOSTS=192.168.32.149:9200  ES_DATE_SEPARATOR=- ES_NODES_WAN_ONLY=true java -jar ./main/target/zipkin-dependencies-1.11.2-SNAPSHOT.jar
